89f02be383ef37f9f4039515ba580496e5bb3fd9
NhatPham94 <nhat.pham@mingle.com>
2018-08-01 17:08:13 +0700

    fix bugs recording state and update version

--- app/src/main/java/com/example/nhatpham/camerafilter/camera/CameraFragment.kt
+++ app/src/main/java/com/example/nhatpham/camerafilter/camera/CameraFragment.kt
@@ -165,7 +165,7 @@ internal class CameraFragment : ViewLifecycleFragment(), View.OnClickListener {
         })
 
         cameraViewModel.recordingStateLiveData.observe(viewLifecycleOwner!!, Observer {
-            if(PREVIEW_TYPE == PreviewType.Both || PREVIEW_TYPE == PreviewType.Video) {
+            if(currentMode == PreviewType.Video) {
                 val active = it == true
                 updateRecordStateView(active)
                 cameraViewModel.isRecording.set(active)
